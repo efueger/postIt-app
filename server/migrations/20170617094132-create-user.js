f41a85a7cf607c597201a098018c7c8955110969
Richard Terungwa Kombol <terungwakombol@gmail.com>
2017-06-18 02:12:24 +0100

    Create database migration for user and message

--- server/migrations/20170617094132-create-user.js
+++ server/migrations/20170617094132-create-user.js
@@ -1,6 +1,6 @@
-'use strict';
+
 module.exports = {
-  up: function(queryInterface, Sequelize) {
+  up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('Users', {
       id: {
         allowNull: false,
@@ -27,7 +27,7 @@ module.exports = {
       }
     });
   },
-  down: function(queryInterface, Sequelize) {
+  down: (queryInterface, Sequelize) => {
     return queryInterface.dropTable('Users');
   }
 };
