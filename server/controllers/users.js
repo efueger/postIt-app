5cf5f899e8dcd5f97c8760f39e4a7a505d917b97
Richard Terungwa Kombol <terungwakombol@gmail.com>
2017-06-18 10:49:13 +0100

    Correct syntax errors

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,15 +1,15 @@
 import user from '../models';
-const User = user.Todo;
+const User = user.User;
 
 module.exports = {
   create(req, res) {
-    return Todo
+    return User
       .create({
         username: req.body.username,
         email: req.body.email,
         password: req.body.password,
       })
-      .then(todo => res.status(201).send(tod0))
+      .then(user => res.status(201).send(user))
       .catch(error => res.status(400).send(error));
   },
 };
