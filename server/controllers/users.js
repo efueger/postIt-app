2a25fc4afb1770a4879dc9d54393f35b62c0800a
Richard Terungwa Kombol <terungwakombol@gmail.com>
2017-06-19 15:13:52 +0100

    Edit createUser class variables

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -2,7 +2,6 @@ import bcrypt from 'bcrypt';
 import user from '../models';
 
 export default class UserHelpers {
-
   /**
   * Updates upvotes array in voteObj
   * @param {object} req for first parameter
@@ -10,17 +9,15 @@ export default class UserHelpers {
   */
   createUser(req, res) {
     const User = user.User;
-    const username = req.body.username;
     const password = req.body.password;
-    const email = req.body.email;
 
     const salt = bcrypt.genSaltSync(10);
     const hashedPassword = bcrypt.hashSync(password, salt);
 
     return User
       .create({
-        username: username,
-        email: email,
+        username: req.body.username,
+        email: req.body.email,
         password: hashedPassword,
       })
       .then(user => res.status(201).send(user))
