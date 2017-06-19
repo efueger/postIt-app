2723eee5dfa7626f2455228a020439cb951022e2
Richard Terungwa Kombol <terungwakombol@gmail.com>
2017-06-19 06:46:39 +0100

    Hash password with bcrypt

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,13 +1,20 @@
+import bcrypt from 'bcrypt';
 import user from '../models';
 const User = user.User;
 
 module.exports = {
   create(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
+
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
     return User
       .create({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
+        username: username,
+        email: email,
+        password: hashedPassword,
       })
       .then(user => res.status(201).send(user))
       .catch(error => res.status(400).send(error));
