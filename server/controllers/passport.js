b4fba89149f3ab4c893bc624e6d731eb6f9836e8
Richard Terungwa Kombol <terungwakombol@gmail.com>
2017-06-19 23:54:02 +0100

    Edit syntax for passport.js

--- server/controllers/passport.js
+++ server/controllers/passport.js
@@ -3,11 +3,49 @@ import localStrategy from 'passport-local';
 import bcrypt from 'bcrypt';
 import user from '../models';
 
+const LocalStrategy = localStrategy.Strategy;
+
 const passportConfig = (app) => {
   // Middleware to initialize passport
   app.use(passport.initialize());
   // Middleware to persist login sessions
   app.use(passport.session());
+  // Configure strategy for application
+  passport.use(new LocalStrategy((username, password, done) =>{
+    user.User.findOne({
+      where: {
+        'username': username
+      }
+    }).then((user) => {
+      if (user == null) {
+        return done(null, false, { message: 'Incorrect credentials.' })
+      }
 
+      const hashedPassword = bcrypt.hashSync(password, user.salt);
 
+      If (user.password === hashedPassword) {
+        return done(null, user);
+      }
+
+      return done(null, false, { message: 'Incorrect credentials.' });
+    })
+  }));
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    user.User.findOne({
+      where: {
+        'id': id
+      }
+    }).then ((user) => {
+      if (user == null) {
+        done(new Error('Wrong user id.'));
+      }
+
+      done(null, user);
+    })
+  })
 }
