aa965cf13f634e3ae7059b7836277b9b836ebf9b
Richard Terungwa Kombol <terungwakombol@gmail.com>
2017-06-20 11:24:33 +0100

    Modify the controller to create user table

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt';
 import user from '../models';
+const User = user.User;
 
 export default class UserHelpers {
   /**
@@ -8,30 +9,25 @@ export default class UserHelpers {
   * @param {object} res for second parameter
   */
   createUser(req, res) {
-    const User = user.User;
     const password = req.body.password;
     const username = req.body.username;
     const email = req.body.email;
     const password2 = req.body.password2;
 
-    if (!username || !password || !password2 || !email) {
-      res.status(201).send("Please, fill in the fields");
-    }
-
-    if (password !== password2) {
-      res.status(201).send("Please, enter the same password twice.");
-    }
-
     const salt = bcrypt.genSaltSync(10);
     const hashedPassword = bcrypt.hashSync(password, salt);
-
-    return User
+    console.log('Username value',User.create);
+    User.sync({force: true}).then(() => {
+      return User
       .create({
         username: username,
         email: email,
         password: hashedPassword,
+        salt: salt
       })
       .then(user => res.status(201).send(user))
       .catch(error => res.status(400).send(error));
+    });
+    
   }
 }
