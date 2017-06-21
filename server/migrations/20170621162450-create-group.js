0cf9818f5c5bf48f67d3e5568db1312b1603a86c
Richard Terungwa Kombol <terungwakombol@gmail.com>
2017-06-21 17:34:50 +0100

    Create migrations for group database models

---
+++ server/migrations/20170621162450-create-group.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Groups', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      groupname: {
+        type: Sequelize.STRING
+      },
+      grouptype: {
+        type: Sequelize.STRING
+      },
+      active: {
+        type: Sequelize.NUMBER
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Groups');
+  }
+};
