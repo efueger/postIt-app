5fe4e3cf6b8dbcac7d811cddb9123fd4ec8408cb
Richard Terungwa Kombol <terungwakombol@gmail.com>
2017-06-23 23:45:16 +0100

    Write initial unit test cases for user model

---
+++ server/test/unit/userSpec.js
@@ -0,0 +1,42 @@
+
+describe('User', () => {
+  it('Should register the Sequelize model', (done) => {
+
+  });
+
+  it('Should load the passport plugin', (done) => {
+
+  });
+
+  it('Should retrieve by username', (done) => {
+
+  });
+
+  describe('username', () => {
+    it('should be required', () => {
+
+    });
+
+    it('should be alphanumberic and have 4-255 chars', () => {
+
+    });
+  })
+
+  describe('email', () => {
+    it('should be required', () => {
+
+    });
+
+    it('should be a string', () => {
+
+    });
+
+    it('should be unique', () => {
+
+    });
+
+    it('should be valid email', () => {
+
+    });
+  });
+})
