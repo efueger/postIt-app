e1919d2c3eaa16b3465b6641b83b2e605d18a942
Richard Terungwa Kombol <terungwakombol@gmail.com>
2017-06-23 14:37:18 +0100

    Implement method to get all created groups

--- server/controllers/group.js
+++ server/controllers/group.js
@@ -1,7 +1,18 @@
-import group from '../models';
-const Group = group.Group;
+import model from '../models';
+const Group = model.Group;
+const UserGroup = model.UserGroups;
 
 export default class GroupHelpers {
+  /**
+  * Get all created groups
+  * @param {object} req for first parameter
+  * @param {object} res for second parameter
+  */
+  getAllGroups(req, res) {
+    Group.findAl({})
+    .then((user) => res.status(200).json(user));
+  }
+  
   /**
   * Create new group
   * @param {object} req for first parameter
