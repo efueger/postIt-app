bc54ceab1a500123a9863778c8732701b519c9a9
Richard Terungwa Kombol <terungwakombol@gmail.com>
2017-06-26 18:16:13 +0100

    Edit gulp css task to compile materialize.scss

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,21 +2,21 @@ import gulp from 'gulp';
 import sass from 'gulp-sass';
 
 const config = {
-  bootstrapDir: './bower_components/bootstrap-sass',
+  bootstrapDir: './bower_components/materialize/sass',
   publicDir: './template/public'
 };
 
 gulp.task('css', () => {
-  return gulp.src('./template/css/app.scss')
+  return gulp.src(config.bootstrapDir + '/materialize.scss')
   .pipe(sass({
-    includePaths: [config.bootstrapDir + '/assets/stylesheets'],
+    includePaths: [config.bootstrapDir + '/_style.scss'],
   }))
   .pipe(gulp.dest(config.publicDir + '/css'));
 });
 
-gulp.task('fonts', () => {
-  return gulp.src(config.bootstrapDir + '/assets/fonts/**/*')
-  .pipe(gulp.dest(config.publicDir + '/fonts'));
-});
+// gulp.task('fonts', () => {
+//   return gulp.src(config.bootstrapDir + '/assets/fonts/**/*')
+//   .pipe(gulp.dest(config.publicDir + '/fonts'));
+// });
 
-gulp.task('default', ['css', 'fonts']);
+gulp.task('default', ['css']);
