d712fc9c20f02cf9a68d717c5c76068431ecc921
Richard Terungwa Kombol <terungwakombol@gmail.com>
2017-06-27 03:14:36 +0100

    Create tasks to compile materialize fonts and js files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,21 +2,26 @@ import gulp from 'gulp';
 import sass from 'gulp-sass';
 
 const config = {
-  bootstrapDir: './bower_components/materialize/sass',
+  bootstrapDir: './bower_components/materialize',
   publicDir: './template/public'
 };
 
 gulp.task('css', () => {
-  return gulp.src(config.bootstrapDir + '/materialize.scss')
+  return gulp.src(config.bootstrapDir + '/sass/materialize.scss')
   .pipe(sass({
-    includePaths: [config.bootstrapDir + '/_style.scss'],
+    includePaths: [config.bootstrapDir + '/sass/_style.scss'],
   }))
   .pipe(gulp.dest(config.publicDir + '/css'));
 });
 
-// gulp.task('fonts', () => {
-//   return gulp.src(config.bootstrapDir + '/assets/fonts/**/*')
-//   .pipe(gulp.dest(config.publicDir + '/fonts'));
-// });
+gulp.task('fonts', () => {
+  return gulp.src(config.bootstrapDir + '/fonts/**/*')
+  .pipe(gulp.dest(config.publicDir + '/fonts'));
+});
 
-gulp.task('default', ['css']);
+gulp.task('js', () => {
+  return gulp.src(config.bootstrapDir + '/js/**/*')
+  .pipe(gulp.dest(config.publicDir + '/js'));
+})
+
+gulp.task('default', ['css', 'fonts', 'js']);
