dbf18b92e5a340f96adbc150728db27244a62c54
Richard Terungwa Kombol <terungwakombol@gmail.com>
2017-06-28 12:43:39 +0100

    Setup webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var config = [{
+  entry: './main.js',
+	
+  output: {
+    filename: './index.js',
+  },
+	
+  devServer: {
+      inline: true,
+      port: 8080
+   },
+	
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+			
+        query: {
+          presets: ['es2015', 'react']
+          }
+         }
+      ]
+   }
+}]
+
+export default config;
