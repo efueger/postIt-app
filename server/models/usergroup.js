9a0e079ae0f4d377306d29aa3d759ab60dd785fe
Richard Terungwa Kombol <terungwakombol@gmail.com>
2017-06-29 23:11:33 +0100

    Edit usergroup database column options

--- server/models/usergroup.js
+++ server/models/usergroup.js
@@ -2,16 +2,6 @@ const userGroup = (sequelize, DataTypes) => {
   const UserGroup = sequelize.define('userGroup', {
     userId: DataTypes.INTEGER,
     groupId: DataTypes.INTEGER
-  }, {
-    classMethods: {
-      associate: (models) => {
-        Message.belongsTo(models.Group, {
-          foreignKey: 'groupId',
-          targetKey: 'messageId',
-          onDelete: 'CASCADE'
-        })
-      }
-    }
   });
   return UserGroup;
 };
