dcaacb049d68f0a8a2e1804010e53bbb289e51dd
Richard Terungwa Kombol <terungwakombol@gmail.com>
2017-06-29 11:38:42 +0100

    Separate test suites for different user actions

--- server/test/functional/userRouteSpec.js
+++ server/test/functional/userRouteSpec.js
@@ -6,69 +6,123 @@ import models from '../../models/';
 const user = models.User;
 
 describe('User-Routes', () => {
-  beforeEach(() => {
-    return user.sync({ force: false }).then(() => {
-      user.bulkCreate(users);
+  describe('Get user', () => {
+    beforeEach(() => {
+      return user.sync({ force: false }).then(() => {
+        user.bulkCreate(users);
+      });
+    });
+
+    afterEach(() => {
+      return user.destroy({ truncate: true, cascade: true });
+    });
+
+    it('should return correct object type for users', (done) => {
+      req(app)
+        .get('/api/user/')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end((err, res) => {
+          if (err) { throw err; }
+          expect(typeof res.body).toEqual('object');
+          done();
+        });
+    });
+
+    it('should return correct object type for a user', (done) => {
+      req(app)
+        .get('/api/user/1')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end((err, res) => {
+          if (err) { throw err; }
+          expect(typeof res.body).toEqual('object');
+          done();
+        });
+    });
+  })
+  
+  describe('User registration', () => {
+    beforeEach(() => {
+      return user.sync({ force: false }).then(() => {
+        user.bulkCreate(users);
+      });
+    });
+
+    afterEach(() => {
+      return user.destroy({ truncate: true, cascade: true });
+    });
+
+    it('should create a new user', (done) => {
+      req(app)
+        .post('/api/user/signup')
+        .send({
+          username: 'myuser',
+          email: 'newuser@example.com',
+          password: '1234',
+          password2: '1234'
+        })
+        .expect(201)
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .end((err, res) => {
+          if (err) { throw err; }
+          expect(res.body.username).toEqual('myuser');
+          done();
+        });
+    });
+  });
+  
+  describe('User log in', () => {
+    beforeEach(() => {
+      return user.sync({ force: false }).then(() => {
+        user.bulkCreate(users);
+      });
+    });
+
+    afterEach(() => {
+      return user.destroy({ truncate: true, cascade: true });
+    });
+
+    it('should sign in a created user', (done) => {
+      req(app)
+        .post('/api/user/login')
+        .send({
+          username: 'johndoe',
+          password: 'johns_password'
+        })
+        .expect(200)
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .end((err, res) => {
+          expect(res.body.status).toEqual('success');
+          done();
+        });
     });
   });
 
-  afterEach(() => {
-    return user.destroy({ truncate: true, cascade: true });
-  });
-
-  it('should return correct object type for users', (done) => {
-    req(app)
-      .get('/api/user/')
-      .expect(200)
-      .expect('Content-Type', /json/)
-      .end((err, res) => {
-        if (err) { throw err; }
-        expect(typeof res.body).toEqual('object');
-        done();
+  describe('User not logged in', () => {
+    beforeEach(() => {
+      return user.sync({ force: false }).then(() => {
+        user.bulkCreate(users);
       });
-  });
+    });
 
-  it('should return correct object type for a user', (done) => {
-    req(app)
-      .get('/api/user/1')
-      .expect(200)
-      .expect('Content-Type', /json/)
-      .end((err, res) => {
-        if (err) { throw err; }
-        expect(typeof res.body).toEqual('object');
-        done();
-      });
-  });
+    afterEach(() => {
+      return user.destroy({ truncate: true, cascade: true });
+    });
 
-  it('should create a new user', (done) => {
-    req(app)
-      .post('/api/user/signup')
-      .send({
-        username: 'myuser',
-        email: 'newuser@example.com',
-        password: '1234',
-        password2: '1234'
-      })
-      .expect(201)
-      .expect('Content-Type', 'application/json; charset=utf-8')
-      .end((err, res) => {
-        expect(res.body.username).toEqual('myuser');
-        done();
-      });
-  });
-
-  it('should sign in a created user', (done) => {
-    req(app)
-      .post('/api/user/login')
-      .send({
-        username: 'myuser',
-        password: '1234'
-      })
-      .expect(200)
-      .expect('Content-Type', 'application/json; charset=utf-8')
-      .end((err, res) => {
-        expect(res.body.status).toEqual('User not found');
-        done();
-      });
-  });
+    it('should not sign in an unregistered user', (done) => {
+      req(app)
+        .post('/api/user/login')
+        .send({
+          username: 'myuser',
+          password: '1234'
+        })
+        .expect(200)
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .end((err, res) => {
+          expect(res.body.status).toEqual('User not found');
+          done();
+        });
+    });
+  })
 });
