2f47e74e0c362e05fad328cdcd06945f3b16ff37
Richard Terungwa Kombol <terungwakombol@gmail.com>
2017-06-30 02:29:41 +0100

    Edit syntax to fix heroku bug

--- server/app.js
+++ server/app.js
@@ -37,7 +37,7 @@ app.get('*', (req, res) => res.status(200).send({
   message: 'Welcome to the begining of nothingness.',
 }));
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT);
 console.log('Listen to app at port...', 3000);
 
 export default app;
