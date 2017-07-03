168255c74664017943f9dd528ec9b519b233e212
Richard Terungwa Kombol <terungwakombol@gmail.com>
2017-07-03 03:17:30 +0100

    Modify user group message class method to post and get user group message

--- server/controllers/message.js
+++ server/controllers/message.js
@@ -3,6 +3,8 @@ import message from '../models';
 
 const Group = model.Group;
 const Message = model.Message;
+const User = model.User;
+const UserGroup = model.UserGroup;
 
 export default class MessageHelpers {
   /**
@@ -13,18 +15,31 @@ export default class MessageHelpers {
   sendMessage(req, res) {
     const groupId = req.params.groupid;
     const messageContent = req.body.content;
-    const messageOwnerId = 3
+    const messageOwner = req.body.username;
 
-    Message.sync({force: false}).then(() => {
-      return Message
-      .create({
-        groupId: groupId,
-        content: messageContent,
-        userId: messageOwnerId
-      })
-      .then(user => res.status(200).send(user));
-    });  
-  }
+    UserGroup.findOne({
+      where: {
+        groupId: groupId
+      }
+    }).then((user) => {
+      UserGroup.findOne({
+        where: {
+          groupId: groupId,
+          userId: user.id
+        }
+      }).then((userGroup) => {
+        Message.sync({force: false}).then(() => {
+          return Message
+          .create({
+            groupId: userGroup.groupId,
+            content: messageContent,
+            userId: userGroup.userId
+          })
+          .then(message => res.status(200).send(message));
+        }); 
+      });     
+    });
+  };
   /**
   * Get All group messages
   * @param {object} req as first parameter
@@ -32,12 +47,18 @@ export default class MessageHelpers {
   */
   getAllGroupMessages(req, res) {
     const groupId = req.params.groupid;
-    Message.findAll({
+    Group.findOne({
       where: {
-        'groupId': groupId
+        id: groupId
       }
-    }).then((message) => {
-      res.status(200).json(message);
-    });
-  }
-}
+    }).then((group) => {
+      Message.findAll({
+        where: {
+          'groupId': groupId
+        }
+      }).then((message) => {
+        res.status(200).json(message);
+      });
+    });   
+  };
+};
